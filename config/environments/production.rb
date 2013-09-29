ba10ba369468851ee2c0a65f35c5511c9ff86b65
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-09-29 11:59:13 +0800

    Changed configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,10 @@ Mbkmrk::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -59,7 +60,7 @@ Mbkmrk::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

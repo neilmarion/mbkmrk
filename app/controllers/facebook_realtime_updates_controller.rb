1b67bcc9ebf2bb7f23ebb52474cc5e8082474f3a
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-10-02 14:51:41 +0800

    Reverted back the realtime updates controller

--- app/controllers/facebook_realtime_updates_controller.rb
+++ app/controllers/facebook_realtime_updates_controller.rb
@@ -1,12 +1,19 @@
 class FacebookRealtimeUpdatesController < ApplicationController
-  VERIFY_TOKEN = 'e9prWj1M6nkc152';
+  VERIFY_TOKEN = "e9prWj1M6nkc152"
 
-  def create
-    Rails.logger.info("UPDATED -------------------------")
-    Rails.logger.info(params)
-  end
-
-  def index
-    render :text=>Koala::Facebook::RealtimeUpdates.meet_challenge(params, VERIFY_TOKEN)
+  def subscription
+    if request.method == "GET"
+      if params['hub.mode'] =='subscribe' && params['hub.verify_token'] == VERIFY_TOKEN
+        render :text => params['hub.challenge']
+      else 
+        render :text => 'Failed to authorize facebook challenge request'
+      end
+    elsif request.method == "POST"
+      Rails.logger.info("update-------------------------------") 
+      Rails.logger.info(JSON.parse(request.body.read))
+      updated_obj = JSON.parse(request.body.read)
+      puts updated_obj
+      render :text => "Thanks for the update"
+    end
   end
 end

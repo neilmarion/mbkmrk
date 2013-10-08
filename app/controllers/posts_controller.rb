22469e080aa4eb0e63a925b7a02d01334b6779f6
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-08 18:56:41 +0800

    Fixed layouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, only: [:index]
   
-  layout 'home'
-
   def index
     @posts = current_user.posts.order('created_at DESC')
 

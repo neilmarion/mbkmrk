baa7b0bd89feb6baf556cd198e03fb7cd1c79ad9
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-08 13:24:52 +0800

    Remove protect_from_forgery

--- app/controllers/concerns/realtimeable.rb
+++ app/controllers/concerns/realtimeable.rb
@@ -3,6 +3,7 @@ module Realtimeable
 
   include do
     skip_before_filter :verify_authenticity_token, :only => [:subscription]
+    protect_from_forgery with: :null_session
   end
 
   def subscription
